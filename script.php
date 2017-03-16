2d5b7efc08096184d67102b6307c77aac9e88b73
arnaud <arnaud.saint-patrice@atm-consulting.fr>
2017-03-16 17:54:50 +0100

    FIX features

--- script.php
+++ script.php
@@ -36,7 +36,7 @@ while (TRUE)
 	// To debug (equivalent to var_dump): error_log(var_export($var, true));
 	$result = '';
 	$Controller->setRound($j);
-	$result = $Controller->choice_attack();
+	$result = $Controller->result_move();
 	// Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
 	echo $result."\n";
 	$j++;
@@ -59,11 +59,12 @@ class CodeInGameController
 		$this->TData = array();
 		$this->TMap = array();
 		// Définition des règles
-		$this->TRules['troopSent'] = 1;
-		$this->TRules['multiplier_defprod'] = 15;
-		$this->TRules['multiplier_defcyborgs'] = 5;
-		$this->TRules['multiplier_distance'] = 5;
-		$this->TRules['min_cyborgs'] = 5;
+		$this->TRules['troopSent'] = 1.2;
+		$this->TRules['multiplier_defprod'] = 5;
+		$this->TRules['multiplier_defcyborgs'] = 1;
+		$this->TRules['multiplier_distance'] = 3;
+		$this->TRules['min_cyborgs'] = 1;
+		$this->TRules['min_instructions'] = 4;
 	}
 
 	function setTData($TData) {
@@ -96,7 +97,7 @@ class CodeInGameController
 					$arg5
 					);
 			$this->TData[$entityType][$arg1][$entityId] = array(2=>$arg2,$arg3,$arg4,$arg5);
-			if($entityType == 'FACTORY') {
+			if($entityType == 'FACTORY' && $arg1 == 1) {
 				$this->TData['nbcyborgs']['total'] += $arg2;
 				$this->TData['nbcyborgs'][$entityId] = $arg2;
 			}
@@ -121,59 +122,146 @@ class CodeInGameController
 		return $source;
 	}
 
-	function choice_attack() {
+	function result_move() {
 		$this->mapping();
-		$ret = '';
-		for($i=1;$i<=1;$i++){
-			if($this->TData['nbcyborgs']['total'] <= 1) {
-				$res = 'WAIT';
-			}else{
-				$source     = $this->getSourceFactory();
-				$nbcyborgs  = $this->TData['FACTORY'][1][$source][2] - 1;
-				if(isset($source) && $nbcyborgs >= $this->TRules['min_cyborgs']) {
-					$TInstruction   = $this->getAtkInstruction($source,$nbcyborgs);
-					if(isset($source) && isset($TInstruction['dest']) && isset($TInstruction['cyborgs'])) {
-						$destination    = $TInstruction['dest'];
-						$nbcyborgs      = $TInstruction['cyborgs'];
-						$this->TData['nbcyborgs']['total'] -= $nbcyborgs;
-						$this->TData['nbcyborgs'][$source] -= $nbcyborgs;
-						$this->TData['FACTORY'][1][$source][2] -= $nbcyborgs;
-						$res = 'MOVE '.$source.' '.$destination.' '.$nbcyborgs;
-					}else{
-						$res = 'WAIT';
-					}
-				} else {
-					$res = 'WAIT';
-				}
+		$TRes = array();
+		if($this->TData['nbcyborgs']['total'] > 0) {
+			if($this->nbCH == 0) {
+				$TRes[] = $this->choice_attack();
+				error_log(var_export('ATK', true));
+			}else {
+				$TRes[] = $this->choice_colon();
+				error_log(var_export('COL', true));
 			}
-			$ret.= $res.';';
+			if($this->nbMY >= 2) {
+				$TRes[] = $this->choice_defense();
+				error_log(var_export('DEF', true));
+			}
+			if($this->round == 1 || $this->round == 60) {
+				$TRes[] = $this->choice_bomb();
+				error_log(var_export('BOMB', true));
+			}
+			while(sizeof($TRes) < $this->TRules['min_instructions']) {
+				$TRes[] = $this->choice_attack();
+				error_log(var_export('WHILE ATK', true));
+			}
+			$res = implode(';',$TRes);
+		}else{
+			$res = 'WAIT';
 		}
-		$ret = substr($ret,0,-1);
-		return $ret;
+		return $res;
 	}
 
-	function getAtkInstruction($source, $nbcyborgs) {
+
+	/*
+	 * Attack Part
+	 */
+
+	function choice_attack() {
+		$source     = $this->getSourceFactory();
+		$nbcyborgs  = $this->TData['FACTORY'][1][$source][2] - 1;
+		if(isset($source) && $nbcyborgs >= $this->TRules['min_cyborgs']) {
+			$TInstruction   = $this->getMoveInstruction(-1,$source,$nbcyborgs);
+			if(isset($source) && isset($TInstruction['dest']) && isset($TInstruction['cyborgs'])) {
+				$destination    = $TInstruction['dest'];
+				$typeEntity     = $TInstruction['dest_type'];
+				$nbcyborgs      = $TInstruction['cyborgs'];
+				$distance       = $TInstruction['distance'];
+				$this->updateMove($source,$nbcyborgs,$destination,$typeEntity,$distance);
+				$res = 'MOVE '.$source.' '.$destination.' '.$nbcyborgs;
+			}else{
+				$res = 'WAIT';
+			}
+		} else {
+			$res = 'WAIT';
+		}
+		return $res;
+	}
+
+
+	/*
+	 * Colonisation Part
+	 */
+
+	function choice_colon() {
+		$source     = $this->getSourceFactory();
+		$nbcyborgs  = $this->TData['FACTORY'][1][$source][2] - 1;
+		if(isset($source) && $nbcyborgs >= $this->TRules['min_cyborgs']) {
+			$TInstruction   = $this->getMoveInstruction(0,$source,$nbcyborgs);
+			if(isset($source) && isset($TInstruction['dest']) && isset($TInstruction['cyborgs'])) {
+				$destination    = $TInstruction['dest'];
+				$typeEntity     = $TInstruction['dest_type'];
+				$nbcyborgs      = $TInstruction['cyborgs'];
+				$distance       = $TInstruction['distance'];
+				$this->updateMove($source,$nbcyborgs,$destination,$typeEntity,$distance);
+				$res = 'MOVE '.$source.' '.$destination.' '.$nbcyborgs;
+			}else{
+				$res = 'WAIT';
+			}
+		} else {
+			$res = 'WAIT';
+		}
+		return $res;
+	}
+
+	/*
+	 * Defense Part
+	 */
+
+	function choice_defense() {
+		$source     = $this->getSourceFactory();
+		$nbcyborgs  = $this->TData['FACTORY'][1][$source][2] - 1;
+		if(isset($source) && $nbcyborgs >= $this->TRules['min_cyborgs']) {
+			$TInstruction   = $this->getMoveInstruction(1,$source,$nbcyborgs);
+			if(isset($source) && isset($TInstruction['dest']) && isset($TInstruction['cyborgs'])) {
+				$destination    = $TInstruction['dest'];
+				$typeEntity     = $TInstruction['dest_type'];
+				$nbcyborgs      = $TInstruction['cyborgs'];
+				$distance       = $TInstruction['distance'];
+				$this->updateMove($source,$nbcyborgs,$destination,$typeEntity,$distance);
+				$res = 'MOVE '.$source.' '.$destination.' '.$nbcyborgs;
+			}else{
+				$res = 'WAIT';
+			}
+		} else {
+			$res = 'WAIT';
+		}
+		return $res;
+	}
+
+
+	/*
+	 * Bomb Part
+	 */
+
+	function choice_bomb() {
+		$res = 'WAIT';
+		return $res;
+	}
+
+
+	/*
+	 * General Part
+	 */
+
+
+	function getMoveInstruction($typeEntity, $source, $nbcyborgs) {
 		$TFactory           = $this->TData['FACTORY'][1];
 		$TFactoryMap        = $this->TMap[$source];
 		$TRes               = array();
 
 		foreach($TFactoryMap as $factory => $distance) {
-			if($factory != $source) {
-				if($this->nbCH > 0) {
-					$TTemp = $this->getDestFor(0, $source, $nbcyborgs, $factory);
-					//error_log(var_export('Round : '.$this->round.' || SUISSE dispo '.$this->nbCH,true));
-				}else{
-					$TTemp = $this->getDestFor(-1, $source, $nbcyborgs, $factory);
-					//error_log(var_export('Round : '.$this->round.' || ATK dispo '.$this->nbVS,true));
-					//error_log(var_export($source,true));
-					//error_log(var_export($TTemp,true));
-				}
+			if(!empty($this->TData['FACTORY'][$typeEntity][$factory])) {
+				$TTemp = $this->getDestFor($typeEntity, $source, $nbcyborgs, $factory); // Get Data
 
+				// Choose priority
 				if(empty($TTemp)) {
-					$TRes = $TRes;
+					// No choice found
 				}else if(empty($TRes)) {
+					// First case
 					$TRes = $TTemp;
 				}else if($TTemp['priority'] > $TRes['priority']) {
+					// Priority > ex Priority
 					$TRes = $TTemp;
 				}
 			}
@@ -185,52 +273,78 @@ class CodeInGameController
 
 	function getDestFor($typeEntity, $source, $nbcyborgs, $factory) {
 		$TRes = array();
-		$troups_on  = 0;
 		if(!empty($this->TData['FACTORY'][$typeEntity][$factory])) {
 			$def_cyborgs        = $this->TData['FACTORY'][$typeEntity][$factory][2];
-			$def_cyborgs        += $this->calcTroopsSent(-1, $factory);;
+			$inc_cyborgs		= $this->calcTroopsOnTheRoad($factory); // Add moves comming
 			$def_prod           = $this->TData['FACTORY'][$typeEntity][$factory][3];
 			$def_length         = $this->TMap[$source][$factory];
 			$priority_test      = 0;
-			if($typeEntity == 0) {
+			if($typeEntity == 1){
+				// Cas MY
+				$cyborgs_needed = (-$def_cyborgs + $inc_cyborgs);
+				$priority_test 	= (($def_prod * $this->TRules['multiplier_defprod'])/$cyborgs_needed);
+			}else if($typeEntity == 0){
 				// Cas SUISSE
-				$cyborgs_needed = $def_cyborgs + $def_prod * $def_length;
-				$priority_test -= ($def_cyborgs * $this->TRules['multiplier_defcyborgs']);
+				$cyborgs_needed = ($def_cyborgs + -$inc_cyborgs);
+				$priority_test -= ($cyborgs_needed * $this->TRules['multiplier_defcyborgs']);
 				$priority_test += ($def_prod * $this->TRules['multiplier_defprod']);
 			}else{
 				// Cas VS
-				$cyborgs_needed = $def_cyborgs + $def_prod * ($def_length + 2);
-				$priority_test -= $def_cyborgs * $this->TRules['multiplier_defcyborgs'];
-				$priority_test += $def_prod * $this->TRules['multiplier_defprod'];
-				$priority_test -= $def_length * $this->TRules['multiplier_distance'];
+				$cyborgs_needed = ($def_cyborgs + $inc_cyborgs + $def_prod * ($def_length + 1));
+				$priority_test -= ($cyborgs_needed * $this->TRules['multiplier_defcyborgs']);
+				$priority_test += ($def_prod * $this->TRules['multiplier_defprod']);
+				$priority_test -= ($def_length * $this->TRules['multiplier_distance']);
 			}
 			$cyborgs_needed += 1;
 
-			if($nbcyborgs >= $cyborgs_needed && $troups_on < $cyborgs_needed ) {
-				$TRes['dest']        = $factory;
-				$TRes['cyborgs']     = $cyborgs_needed;
-				$TRes['priority']    = $priority_test;
+			if($nbcyborgs >= $cyborgs_needed && $cyborgs_needed > 1) {
+				$TRes['dest']       = $factory;
+				$TRes['cyborgs']    = $cyborgs_needed;
+				$TRes['dest_type']  = $typeEntity;
+				$TRes['distance']   = $def_length;
+				$TRes['priority']   = $priority_test;
 				//error_log(var_export($TRes,true));
 			}
 		}
 		return $TRes;
 	}
 
+	function updateMove($source,$nbcyborgs,$dest,$distance) {
+		$this->TData['nbcyborgs']['total'] -= $nbcyborgs;
+		$this->TData['nbcyborgs'][$source] -= $nbcyborgs;
+		$this->TData['FACTORY'][1][$source][2] -= $nbcyborgs;
+		$troop = array(
+				1=>1,
+				2=>$source,
+				3=>$dest,
+				4=>$nbcyborgs,
+				5=>$distance
+		);
 
-	function calcTroopsSent($typeEntity, $dest) {
+		$this->TData['TROOP'][1][] = $troop;
+	}
+
+	function calcTroopsOnTheRoad($dest) {
 		$troups_on = 0;
-		if(!empty($this->TData['TROOP'][$typeEntity])) {
-			$TTroops = $this->TData['TROOP'][$typeEntity];
+		if(!empty($this->TData['TROOP'][-1])) {
+			// Calc nb VS troops comming
+			$TTroops = $this->TData['TROOP'][-1];
+			foreach($TTroops as $troup){
+				if($troup[3] == $dest) {
+					$troups_on -= $troup[4];
+				}
+			}
+		}
+		if(!empty($this->TData['TROOP'][1])) {
+			// Calc nb MY troops comming
+			$TTroops = $this->TData['TROOP'][1];
 			foreach($TTroops as $troup){
 				if($troup[3] == $dest) {
 					$troups_on += $troup[4];
 				}
 			}
 		}
-		// Règle d'envoi
-		if($typeEntity == 1)
-			$troups_on *= $this->TRules['troopSent'];
-			return $troups_on;
+		return $troups_on;
 	}
 
 
